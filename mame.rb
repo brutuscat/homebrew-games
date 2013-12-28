94d6d3da64647eb7c874f99f973ec724da449026
Mauro Asprea <mauroasprea@gmail.com>
2013-12-28 21:04:23 +0100

    Patch

--- mame.rb
+++ mame.rb
@@ -7,6 +7,10 @@ class Mame < Formula
 
   head 'git://git.redump.net/mame'
 
+  def patches
+    "https://gist.github.com/brutuscat/8163526/raw/a37fddc7c6a7c7e1bab6f2d9f064940dfa9da63b/sdl2-patch.diff"
+  end
+  
   depends_on :x11
   depends_on 'sdl'
 
